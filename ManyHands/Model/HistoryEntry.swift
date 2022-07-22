1dc2528a22f54b082d35359938f8241feed5e9b7
Kyrill Cousson <kyrill.cousson@gmail.com>
2022-07-22 12:02:31 +0300

    Updated Model

--- ManyHands/Model/HistoryEntry.swift
+++ ManyHands/Model/HistoryEntry.swift
@@ -7,11 +7,20 @@
 
 import Foundation
 
-struct HistoryEntry {
-    let id:String
+struct HistoryEntry:Codable, CustomStringConvertible {
+    var description: String { "HistoryEntry(productId:\(productId), userId:\(userId), entryDate:\(entryDate.description), imageUrl:\(imageUrl ?? "nil"), text:\(text ?? "nil"))" }
+
     let productId:String
     let userId:String
     var text:String?
     var imageUrl:String?
     var entryDate:Date
+    
+    enum CodingKeys: String, CodingKey {
+        case productId
+        case userId
+        case text
+        case imageUrl = "image"
+        case entryDate
+    }
 }
--- ManyHands/Model/Product.swift
+++ ManyHands/Model/Product.swift
@@ -11,6 +11,7 @@ struct Product:Codable, CustomStringConvertible {
     var description: String { "Product(humanReadableId:\(humanReadableId ?? "nil"), name:\(name ?? "nil"))" }
     
     let humanReadableId:String?
+    let isPublic:Bool?
     var name:String?
     var productDescription:String?
     var imageUrl:String?
@@ -18,8 +19,11 @@ struct Product:Codable, CustomStringConvertible {
     var year:Int?
     var ownerUserId:String? // Could be unused if we refer to product.owners.where(owningFromDate < Date() && owningUntilDate == nil)
     
+    var historyEntries : [HistoryEntry]?
+    
     enum CodingKeys: String, CodingKey {
         case humanReadableId
+        case isPublic
         case name
         case productDescription = "description"
         case imageUrl = "image"
