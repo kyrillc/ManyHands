41a9ee16f7abb8e9b27f85f5b8a1d520c2d82029
Kyrill Cousson <kyrill.cousson@gmail.com>
2022-07-26 22:51:55 +0300

    Updated project structure and added NewProductPage

---
+++ ManyHands/ViewControllers/NewProductPage/NewProductViewController.swift
@@ -0,0 +1,14 @@
+//
+//  NewProductViewController.swift
+//  ManyHands
+//
+//  Created by Kyrill Cousson on 26/07/2022.
+//
+
+import UIKit
+import SnapKit
+import RxSwift
+
+class NewProductViewController: UIViewController {
+    
+}
